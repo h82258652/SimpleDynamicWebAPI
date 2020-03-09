017c50a4cf80f40ec92aa2edc17585de28a33ce8
h82258652 <842053625@qq.com>
2020-03-09 21:31:43 +0800

    create ApplicationServiceControllerFeatureProvider

---
+++ src/SimpleDynamicWebAPI/ApplicationServiceControllerFeatureProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using System.Reflection;
+
+namespace SimpleDynamicWebAPI
+{
+    public class ApplicationServiceControllerFeatureProvider : ControllerFeatureProvider
+    {
+        protected override bool IsController(TypeInfo typeInfo)
+        {
+            if (typeof(IApplicationService).IsAssignableFrom(typeInfo))
+            {
+                if (!typeInfo.IsInterface &&
+                    !typeInfo.IsAbstract &&
+                    !typeInfo.IsGenericType &&
+                    typeInfo.IsPublic)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}

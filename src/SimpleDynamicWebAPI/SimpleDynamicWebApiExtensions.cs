ab0ca187d6caed2dd75f27e5e59439f42e7dd639
h82258652 <842053625@qq.com>
2020-03-09 21:54:22 +0800

    SimpleDynamicWebApiExtensions first version

---
+++ src/SimpleDynamicWebAPI/SimpleDynamicWebApiExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SimpleDynamicWebAPI
+{
+    public static class SimpleDynamicWebApiExtensions
+    {
+        public static IMvcBuilder AddDynamicWebApi(this IMvcBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.ConfigureApplicationPartManager(applicationPartManager =>
+            {
+                applicationPartManager.FeatureProviders.Add(new ApplicationServiceControllerFeatureProvider());
+            });
+            return builder;
+        }
+
+        public static IMvcCoreBuilder AddDynamicWebApi(this IMvcCoreBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.ConfigureApplicationPartManager(applicationPartManager =>
+            {
+                applicationPartManager.FeatureProviders.Add(new ApplicationServiceControllerFeatureProvider());
+            });
+            return builder;
+        }
+    }
+}

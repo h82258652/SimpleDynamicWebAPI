9a04f4a2b60df9643ce32ee86cdcaedbcc55e71f
h82258652 <842053625@qq.com>
2020-03-09 21:56:38 +0800

    AddDynamicWebApi after AddControllers

--- demo/Demo/Startup.cs
+++ demo/Demo/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SimpleDynamicWebAPI;
 
 namespace Demo
 {
@@ -24,7 +25,7 @@ namespace Demo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddDynamicWebApi();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
